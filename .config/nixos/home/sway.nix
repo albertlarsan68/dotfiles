acd0e09462b0a6cb968afe9cc3a7c235d9d59039
Albert Larsan <albertlarsan@albertlarsan.fr>
2025-03-21 18:39:19 +0100

    Configure Sway

--- .config/nixos/home/sway.nix
+++ .config/nixos/home/sway.nix
@@ -1,14 +1,24 @@
 { lib, pkgs, ... }:
 {
+  # home.pointerCursor.sway.enable = true;
   programs.kitty = {
     enable = true;
   };
+  programs.swaylock.enable = true;
+  programs.waybar.enable = true;
   wayland.windowManager.sway = {
     enable = true;
+    checkConfig = true;
     config = rec {
+      defaultWorkspace = "workspace number 1";
       modifier = "Mod4";
       # Use kitty as default terminal
-      terminal = "kitty";
+      terminal = "${pkgs.kitty}/bin/kitty";
+      bars = [
+        {command = "${pkgs.waybar}/bin/waybar";}
+      ];
     };
+    systemd.xdgAutostart = true;
+    wrapperFeatures.gtk = true;
   };
 }
